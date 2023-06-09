aee9e5f6e9d098127c0816200c4d8d665935b5bb
olinicholson <olinicholson4@gmail.com>
2023-06-09 12:47:29 -0300

    script3 agregado

--- script.js
+++ script.js
@@ -1,30 +1,84 @@
-d3.csv('final_visualizacion_LAWSON_VALEGGIA_NICHOLSON/datos/datos_agrupados_OLIVIA.csv', d3.autoType).then(data => {
-  // Ordenar los datos por msPlayed de forma descendente y tomar las 10 primeras canciones
-  const filteredData = data.sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 10);
-  
-  filteredData.forEach(d => {
+d3.csv('datos/datos_actualizados_LUISA.csv', d3.autoType).then(data => {
+    data.forEach(d => {
       const fecha_hora = d.endTime.split(' ');
-      const hora = fecha_hora[1].split(':')[0];
-      d.hour = hora;
-      d.msPlayed = d.msPlayed / (1000 * 60 * 60); // Convertir los milisegundos a horas
-  });
-
-  const sums = d3.rollup(filteredData, v => d3.sum(v, d => d.msPlayed), d => d.hour);
-
-  const chart_1 = Plot.plot({
+      const mes = fecha_hora[0].split('-')[1];
+      d.month = mes;
+      d.hoursPlayed = d.msPlayed / (1000 * 60 * 60); // Convertir los milisegundos a horas
+    });
+    const sums = d3.rollup(data, v => d3.sum(v, d => d.hoursPlayed), d => d.month);
+    const chart2 = Plot.plot({
       marginLeft: 60,
       marginRight: 60,
       grid: true,
-      x: { label: 'Hora' },
-      y: { label: 'Cantidad de canciones escuchadas (horas)', domain: [0, 24] },
+      x: { label: 'Mes' , domain: ["01","02","03","04","05","06","07","08","09","10","11","12"] },
+      y: { label: 'Cantidad de horas de música escuchadas', domain:[0,35] },
       color: { legend: false },
       marks: [
-          Plot.barY(sums,
-              { x: d => d[0], y: d => d[1], fill: '#225EA8' }
-          ),
-          Plot.frame()
+        Plot.barY(sums,
+          { x: d => d[0], y: d => d[1], fill: '#225EA8' }
+        ),
+        Plot.frame()
       ]
+    });
+
+    d3.select('#chart_1_luisa').append(() => chart2);
   });
 
-  d3.select('#chart_3_olivia').append(() => chart_1);
-});
+
+  d3.csv('datos/datos_actualizados_SOLVA.csv', d3.autoType).then(data => {
+    data.forEach(d => {
+      const fecha_hora = d.endTime.split(' ');
+      const mes = fecha_hora[0].split('-')[1];
+      d.month = mes;
+      d.hoursPlayed = d.msPlayed / (1000 * 60 * 60); // Convertir los milisegundos a horas
+    });
+
+    const sums = d3.rollup(data, v => d3.sum(v, d => d.hoursPlayed), d => d.month);
+
+    const chart2 = Plot.plot({
+      marginLeft: 60,
+      marginRight: 60,
+      grid: true,
+      x: { label: 'Mes' },
+      y: { label: 'Cantidad de horas de música escuchadas',domain:[0,35] },
+      color: { legend: false },
+      marks: [
+        Plot.barY(sums,
+          { x: d => d[0], y: d => d[1], fill: '#225EA8' }
+        ),
+        Plot.frame()
+      ]
+    });
+
+    d3.select('#chart_1_solva').append(() => chart2);
+  });
+
+
+  d3.csv('datos/datos_actualizados_OLIVIA.csv', d3.autoType).then(data => {
+    data.forEach(d => {
+      const fecha_hora = d.endTime.split(' ');
+      const mes = fecha_hora[0].split('-')[1];
+      d.month = mes;
+      d.hoursPlayed = d.msPlayed / (1000 * 60 * 60); // Convertir los milisegundos a horas
+    });
+
+    const sums = d3.rollup(data, v => d3.sum(v, d => d.hoursPlayed), d => d.month);
+
+    const chart2 = Plot.plot({
+      marginLeft: 60,
+      marginRight: 60,
+      grid: true,
+      x: { label: 'Mes' },
+      y: { label: 'Cantidad de horas de música escuchadas',domain:[0,35] },
+      color: { legend: false },
+      marks: [
+        Plot.barY(sums,
+          { x: d => d[0], y: d => d[1], fill: '#225EA8' }
+        ),
+        Plot.frame()
+      ]
+    });
+
+    d3.select('#chart_1_olivia').append(() => chart2);
+  });
+
