53215a3f2545fded4b779bb34c626b003dd97831
luisalawson <luisalawson14@gmail.com>
2023-06-13 09:30:25 -0300

    initial commit

--- script3.js
+++ script3.js
@@ -1,101 +1,64 @@
 d3.csv('datos/datos_agrupados_OLIVIA.csv', d3.autoType).then(data => {
-    // Ordenar los datos por msPlayed de forma descendente y tomar las 10 primeras canciones
-    const filteredData = data.sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 10);
-    
+    const filteredData = data.sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 5);
     filteredData.forEach(d => {
         d.msPlayed = d.msPlayed / (1000 * 60 * 60); // Convertir los milisegundos a horas
     });
-
     const sums = d3.rollup(filteredData, v => d3.sum(v, d => d.msPlayed), d => d.trackName);
-
     const chart_3 = Plot.plot({
         style: {
             background: '#141414',
             fontSize: 10,
           },
-        width: 650, // Increase the chart width
-        height: 400, // Increase the chart height
-        font: 'Poppins', // Set font family to Poppins
-        marginLeft: 200,
-        marginRight: 60,
-        x: { domain:[0,8]  },
-        y: { label: 'Cantidad de canciones escuchadas (horas)' },
-        color: { legend: false },
+        x: {padding: 0.4},
+        y:{domain:[0,8]},
         marks: [
-            Plot.barX(sums,
-                { y: d => d[0], x: d => d[1], fill: '#225EA8',        sort: { y: "x", reverse: true },  
-            }
-            )
+          Plot.barY(sums, {x: d => d[0], y: d => d[1], dx: 2, dy: 2, sort: { x: "y", reverse: true }}),
+          Plot.barY(sums, {x: d => d[0], y: d => d[1], fill: "#D32373", dx: -2, dy: -2})
         ]
-    });
-
+      })
     d3.select('#chart_3_olivia').append(() => chart_3);
 });
 
 d3.csv('datos/datos_agrupados_LUISA.csv', d3.autoType).then(data => {
-    // Ordenar los datos por msPlayed de forma descendente y tomar las 10 primeras canciones
-    const filteredData = data.sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 10);
-    
+    const filteredData = data.sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 5);
     filteredData.forEach(d => {
         d.msPlayed = d.msPlayed / (1000 * 60 * 60); // Convertir los milisegundos a horas
     });
-
     const sums = d3.rollup(filteredData, v => d3.sum(v, d => d.msPlayed), d => d.trackName);
-
     const chart_3 = Plot.plot({
         style: {
             background: '#141414',
             fontSize: 10,
           },
-        width: 650, // Increase the chart width
-        height: 400, // Increase the chart height
-        font: 'Poppins', // Set font family to Poppins
-        marginLeft: 200,
-        marginRight: 60,
-        x: { domain:[0,8]  },
-        y: { label: 'Cantidad de canciones escuchadas (horas)' },
-        color: { legend: false },
+        x: {padding: 0.4},
+        y:{domain:[0,8]},
         marks: [
-            Plot.barX(sums,
-                { y: d => d[0], x: d => d[1], fill: '#225EA8',         sort: { y: "x", reverse: true },  
-            }
-            )
-            
+          Plot.barY(sums, {x: d => d[0], y: d => d[1], dx: 2, dy: 2, sort: { x: "y", reverse: true }}),
+          Plot.barY(sums, {x: d => d[0], y: d => d[1], fill: "#6823D3", dx: -2, dy: -2})
         ]
-    });
+      })
 
     d3.select('#chart_3_luisa').append(() => chart_3);
 });
 
 d3.csv('datos/datos_agrupados_SOLVA.csv', d3.autoType).then(data => {
-    const filteredData = data.sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 10);
-    
+    const filteredData = data.sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 5);
     filteredData.forEach(d => {
         d.msPlayed = d.msPlayed / (1000 * 60 * 60); // Convertir los milisegundos a horas
     });
-
     const sums = d3.rollup(filteredData, v => d3.sum(v, d => d.msPlayed), d => d.trackName);
-
     const chart_3 = Plot.plot({
         style: {
             background: '#141414',
             fontSize: 10,
           },
-        width: 650, // Increase the chart width
-        height: 400, // Increase the chart height
-        font: 'Poppins', // Set font family to Poppins
-        marginLeft: 200,
-        marginRight: 60,
-        x: { domain:[0,8]  },
-        y: { label: 'Cantidad de canciones escuchadas (horas)' },
-        color: { legend: false },
+        x: {padding: 0.4},
+        y:{domain:[0,8]},
         marks: [
-            Plot.barX(sums,
-                { y: d => d[0], x: d => d[1], fill: '#225EA8',        sort: { y: "x", reverse: true },  
-            }
-            )
-            
+          Plot.barY(sums, {x: d => d[0], y: d => d[1], dx: 2, dy: 2, sort: { x: "y", reverse: true }}),
+          Plot.barY(sums, {x: d => d[0], y: d => d[1], fill: "#23B0D3", dx: -2, dy: -2})
         ]
-    });
+      })
     d3.select('#chart_3_solva').append(() => chart_3);
 });
+
--- script5.js
+++ script5.js
@@ -1,24 +1,50 @@
 d3.csv('datos/datos_merge_coincidencias.csv', d3.autoType).then(data => {
   const filteredData = data.sort((a, b) => b.coincidencias - a.coincidencias).slice(0, 10);
-  const filteredData2 = filteredData.sort;
+
+  function getImageUrl(artistName) {
+    if (artistName === "WOS") {
+      return "imagenes/wos.webp";
+    } else if (artistName === "Bizarrap") {
+      return "imagenes/bizarrap.webp";
+    } else if (artistName === "Nicki Nicole") {
+      return "imagenes/nicki.webp";
+    } else if (artistName === "Morat") {
+      return "imagenes/morat.jpeg";
+    } else if (artistName === "TINI") {
+      return "imagenes/tini.webp";
+    } else {
+      return "imagenes/tini.webp"; // Si no hay imagen específica, usa una imagen predeterminada
+    }
+  }
+
   const chart = Plot.plot({
     style: {
       background: '#141414',
       fontSize: 10,
     },
-    width: 650, // Increase the chart width
-    height: 400, // Increase the chart height
-    font: 'Poppins', // Set font family to Poppins
+    width: 650, // Aumenta el ancho del gráfico
+    height: 400, // Aumenta la altura del gráfico
+    font: 'Poppins', // Establece la fuente a Poppins
     marginLeft: 130,
     marks: [
       Plot.barX(filteredData, {
         sort: { y: "x", reverse: true },  
         x: 'coincidencias',
         y: 'artistName',
-        fill: d => (d.artistName === "WOS" || d.artistName === "Bizarrap"||d.artistName === "Nicki Nicole"||d.artistName === "Morat"||d.artistName === "TINI") ? "#4D36DB" : "#C7D9F1",
+        fill: d => (d.artistName === "WOS" || d.artistName === "Bizarrap" || d.artistName === "Nicki Nicole" || d.artistName === "Morat" || d.artistName === "TINI") ? "#4D36DB" : "#C7D9F1",
+      }),
+      Plot.image(filteredData, {
+        x: 'coincidencias',
+        y: 'artistName',
+        url: d => getImageUrl(d.artistName),
+        width: 20,
+        height: 20,
+        dy: 5,
       })
     ]
   });
 
   d3.select('#chart_8').append(() => chart);
+}).catch(error => {
+  console.log(error);
 });
